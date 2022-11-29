510305bf671bb5008df8bc0c9f39332909ab0a31
Md Mahfuz Rahman Nirob <105499709+mdmahfuz307@users.noreply.github.com>
2022-11-29 21:03:49 +0600

    Update DIU_Food_Court.c

--- DIU Food Court/DIU_Food_Court.c
+++ DIU Food Court/DIU_Food_Court.c
@@ -7,13 +7,14 @@ void main(){
 	int n,ch,cd=0,ci=0,cp=0,orderT=0,itemid,choice,qty;
 
 	while(1)
-	{	A:printf("-----<Food Order for Akash Restaurant>-----\n");
+	{	A:printf("-----<Food Order for DIU Food Court>-----\n");
 		printf("\n\n\t***** Main Menu *****\n");
 		printf("\t1.Create order\n");
 		printf("\t2.Modify Order\n");
 		printf("\t3.Generate bill\n");
 		printf("\t4.Cancel order\n");
 		printf("\t5.Exit\n");
+
 		ch=input();
 		system("cls"); 				//Clear screen in DEV-C++ 5.11 v
 		switch(ch)
@@ -21,7 +22,7 @@ void main(){
 			case 1:D:printf("\n\nSelect the food item\n");
 					printf("Item Name 	Price(Inclusive of all Taxes)\n");
 					printf("-----------------------\n");
-					printf("1.Pizza         499tk\n2.Burger         250tk\n3.Puri          40tk\n4.Return to Main Menu\n");
+					printf("1.Pizza         499tk\n2.Burger         250tk\n3.Puri          40tk\n4.HotDog         299tk\n5.Fries           200tk\n6.Return to Main Menu\n");
 					n=input();
 					switch(n)
 					{
@@ -43,7 +44,21 @@ void main(){
 						goto D;	
 						break;
 
-						case 4:system("cls");
+						case 4:cp++;orderT++;
+						system("cls");
+						printf("HotDog is ordered: Qty=%d\n",cp);
+						goto D;	
+						break;
+
+						case 5:cp++;orderT++;
+						system("cls");
+						printf("Fries is ordered: Qty=%d\n",cp);
+						goto D;	
+						break;
+
+
+
+						case 6:system("cls");
 						goto A;
 						default:printf("Invalid option\n");
 						scanf("%*s");         //Prevents the possibility of infinite loop when "."(dot) is entered as input for the switch case.
@@ -70,7 +85,15 @@ void main(){
 				{
 						printf("113  	  PURI			%d\n",cp);
 				}
-				
+				        if(ci>0)
+				{
+						printf("114       HotDog			%d\n",ci);
+				}
+				        if(ci>0)
+				{
+						printf("115       Fries			%d\n",ci);
+				}
+
 				B:while(1)
 				{	printf("\nEnter your choice\n");
 					printf("1.Alter order\n");
@@ -95,6 +118,7 @@ void main(){
 						goto B;
 					}
 					break;
+
 				case 112:if(qty<=ci && qty>=0)
 					{ci=ci-qty;
 					printf("Idli item deleted: %d \n",qty);
@@ -104,6 +128,7 @@ void main(){
 						goto B;
 					}
 					break;
+
 				case 113:if(qty<=cp && qty>=0)
 					{cp=cp-qty;
 					printf("Puri item deleted: %d\n ",qty);
@@ -113,13 +138,36 @@ void main(){
 						goto B;
 					}
 					break;
+
+				case 114:if(qty<=cp && qty>=0)
+					{cp=cp-qty;
+					printf("HotDog item deleted: %d\n ",qty);
+					}
+					else{
+						printf("Quantity Limit is exceeded or invalid.Try Again\n");
+						goto B;
+					}
+					break;
+ 
+				case 115:if(qty<=cp && qty>=0)
+					{cp=cp-qty;
+					printf("Fries item deleted: %d\n ",qty);
+					}
+					else{
+						printf("Quantity Limit is exceeded or invalid.Try Again\n");
+						goto B;
+					}
+					break;
+
 				default:printf("Invalid Item id.Try again\n");
 				goto B;	
 				}
 					break;
+
 			case 2:system("cls");
 			goto A;
 			break;
+
 			default:printf("Invalid choice!!!\n");
 			scanf("%*s");
 					goto B;		
@@ -132,6 +180,7 @@ void main(){
 					goto A;
 					}
 					break;
+
 			case 3:if(orderT>0)
 					{
 				printf("			DIU FOOD COURT\n    	DIU,SMART CITY,AB-411\n			MOB: +88017805811\n\n");
@@ -139,7 +188,7 @@ void main(){
 				printf("-------------------------------------------------------------------\n");
 				if(cd>0)
 				{
-				printf("   PIZZA	%d	 X  	 499		   %d\n",cd,cd*499);
+				printf("   PIZZA	    %d	 X       	 499		   %d\n",cd,cd*499);
 				}
 				if(ci>0)
 				{
@@ -147,10 +196,20 @@ void main(){
 				}
 				if(cp>0)
 				{
-				printf("   PURI		%d	 X  	  40		   %d\n",cp,cp*40);
+				printf("   PURI	        	%d	 X  	  40		   %d\n",cp,cp*40);
 				}
+				if(cp>0)
+				{
+				printf("   HotDog		%d	 X  	  299		   %d\n",cp,cp*299);
+				}
+				if(cp>0)
+				{
+				printf("   Fries		%d	 X  	  200		   %d\n",cp,cp*200);
+				}
+
+
 				printf("===================================================================\n");
-				printf("				 SUB TOTAL:    %d Tk\n\n",cd*499+ci*250+cp*40);
+				printf("		      	  SUB TOTAL   :              %d Tk\n\n",cd*499+ci*250+cp*40+cp*299+cp*200);
 				printf("\t\t<---THANK YOU FOR YOUR VISIT--->\n");
 				}
 				else
@@ -159,6 +218,7 @@ void main(){
 					goto A;
 				}
 				break;
+
 			case 4:if(orderT>0)
 				{cd=0;ci=0;cp=0;orderT=0;
 				printf("Order is cancelled, Thank You.\n");
@@ -168,18 +228,22 @@ void main(){
 				goto A;
 				}
 				break;
+
 			case 5:
 				cd=0;cp=0;ci=0;orderT=0;
 				printf("Have a Good Day!!!");
 				exit(0);
+
 			default:printf("Invalid Entry.Please try again.!!!\n");
 			scanf("%*s");
 			goto A;
 			break;
+			
 			}		
 		}
 	}
 	int input() 
+
 { 
     int number; 
     scanf("%d", &number); 
